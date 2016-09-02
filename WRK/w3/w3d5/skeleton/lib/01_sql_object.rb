9a409d084d9adb5d25bde551d1aee1a9bc1465dc
Joe <jmk742@nyu.edu>
2016-09-02 16:17:59 -0400

    Make record and make searchable

--- WRK/w3/w3d5/skeleton/lib/01_sql_object.rb
+++ WRK/w3/w3d5/skeleton/lib/01_sql_object.rb
@@ -4,54 +4,159 @@ require 'active_support/inflector'
 # of this project. It was only a warm up.
 
 class SQLObject
+
   def self.columns
     # ...
+    return @columns if @columns
+    ar_of_columns = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name};
+    SQL
+    ar_of_columns = ar_of_columns[0]
+    ar_of_columns.each_with_index do |el, i|
+      ar_of_columns[i] = el.to_sym
+    end
+    @columns = ar_of_columns
   end
 
   def self.finalize!
+
+    columns.each do |method|
+      method = method.to_s
+      define_method(method){
+        self.attributes[method.to_sym]
+      }
+
+      define_method("#{method}=") do |val|
+        self.attributes[method.to_sym] = val
+      end
+    end
+
   end
 
   def self.table_name=(table_name)
     # ...
+    @table_name = table_name
   end
 
   def self.table_name
     # ...
+    if @table_name
+      @table_name
+    else
+      self.to_s.tableize
+    end
   end
 
   def self.all
     # ...
+    hash = DBConnection.execute(<<-SQL)
+      SELECT
+        #{self.table_name}.*
+      FROM
+        #{self.table_name};
+    SQL
+    self.parse_all(hash)
   end
 
   def self.parse_all(results)
     # ...
+    results = results.map { |hash| self.new(hash) }
   end
 
   def self.find(id)
     # ...
+    result = DBConnection.execute(<<-SQL, id)
+      SELECT
+        #{self.table_name}.*
+      FROM
+        #{self.table_name}
+      WHERE
+        #{self.table_name}.id = ?
+      LIMIT
+        1;
+    SQL
+    return self.new(result[0]) if result[0]
+    nil
   end
 
   def initialize(params = {})
     # ...
+    if params == {}
+      return self
+    end
+    hash = send(:attributes)
+
+    params.each do |attr_name, value|
+      if self.class.columns.include?(attr_name.to_sym)
+        attr_name = "#{attr_name}="
+        attr_name = attr_name.to_sym
+        send(attr_name, value)
+      else
+        raise "unknown attribute '#{attr_name}'"
+      end
+    end
+
+    self
   end
 
   def attributes
     # ...
+    @attributes = {} if @attributes.nil?
+    @attributes
   end
 
   def attribute_values
     # ...
+    self.class.columns.map { |col| send(col.to_sym) }
   end
 
   def insert
     # ...
+    cols = self.class.columns
+    question_marks = ["?"] * cols.length
+    question_marks = question_marks.join(", ")
+    col_names = cols.join(", ")
+    values =  attribute_values
+
+    self.id = DBConnection.last_insert_row_id
+    values[0] = self.id
+
+    ar_of_columns = DBConnection.execute(<<-SQL, *values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks});
+    SQL
+
   end
 
   def update
     # ...
+    cols = self.class.columns
+    col_names = cols.join(" = ?, ").concat(" = ?")
+    values =  attribute_values
+    p col_names
+    p values
+
+    ar_of_columns = DBConnection.execute(<<-SQL, *values, self.id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{col_names}
+      WHERE
+        id = ?;
+    SQL
   end
 
   def save
     # ...
+    if self.id == nil
+      insert
+    else
+      update
+    end
   end
 end
