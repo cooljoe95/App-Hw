764eec4e5b07801aafa314bffdf83c3d7cf17936
Joe <jmk742@nyu.edu>
2016-09-02 17:31:59 -0400

    TODO: Phase IIIb

--- WRK/w3/w3d5/skeleton/lib/03_associatable.rb
+++ WRK/w3/w3d5/skeleton/lib/03_associatable.rb
@@ -11,22 +11,41 @@ class AssocOptions
 
   def model_class
     # ...
+    self.class_name.to_s.constantize
   end
 
   def table_name
     # ...
+    class_name.constantize.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     # ...
+    self.foreign_key = name.to_s.foreign_key.to_sym
+    self.class_name = name.to_s.classify
+
+    self.primary_key = :id
+
+    options.each do |key, val|
+      key = key.to_s.concat("=").to_sym
+      self.send(key, val)
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     # ...
+    self.primary_key = :id
+    self.class_name = name.to_s.classify
+    self.foreign_key = self_class_name.foreign_key.to_sym
+
+    options.each do |key, val|
+      key = key.to_s.concat("=").to_sym
+      self.send(key, val)
+    end
   end
 end
 
@@ -34,6 +53,14 @@ module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
     # ...
+    options = BelongsToOptions.new(name, options)
+    p name
+    define_method("#{name}".to_sym){
+      foreign_key = options.foreign_key
+      model_class = options.model_class
+      hash = {options.primary_key => foreign_key}
+      model_class.where(hash)
+    }
   end
 
   def has_many(name, options = {})
@@ -47,4 +74,5 @@ end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
