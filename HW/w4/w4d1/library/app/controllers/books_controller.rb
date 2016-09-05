66988e6c8b365c9d9476c1a705dea35ff2163d94
Joe Katz <jmk742@nyu.edu>
2016-09-04 21:43:31 -0400

    Rails app working

--- HW/w4/w4d1/library/app/controllers/books_controller.rb
+++ HW/w4/w4d1/library/app/controllers/books_controller.rb
@@ -1,18 +1,26 @@
 class BooksController < ApplicationController
   def index
     # your code here
+    @books = Book.all
   end
 
   def new
     # your code here
+    # book = Book.new(book_params)
+    # book.save
   end
 
   def create
     # your code here
+    book = Book.new(book_params)
+    book.save
+    redirect_to "http://localhost:3000/books"
   end
 
   def destroy
     # your code here
+    Book.delete(params["id"].to_i)
+    redirect_to "http://localhost:3000/books"
   end
 
   private
