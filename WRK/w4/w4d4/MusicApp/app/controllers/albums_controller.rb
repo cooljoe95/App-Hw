bcc974b433a3dbd6d9e13d8a1ea255b277bf48c4
App Academy Student <jmk742@nyu.edu>
2016-09-08 18:26:18 -0400

    most relations work

--- WRK/w4/w4d4/MusicApp/app/controllers/albums_controller.rb
+++ WRK/w4/w4d4/MusicApp/app/controllers/albums_controller.rb
@@ -1,11 +1,20 @@
 class AlbumsController < ApplicationController
 
   def new
-
+    @album = Album.new
+    # debugger
+    @album.band_id = params[:band_id]
+    render :new
   end
 
   def create
-
+    album = Album.new(album_params)
+    # debugger
+    if album.save
+      redirect_to album_url(album.id)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -17,7 +26,12 @@ class AlbumsController < ApplicationController
   end
 
   def show
-
+    begin
+      @album = Album.find(params[:id])
+      render :show
+    rescue
+      redirect_to bands_url
+    end
   end
 
   def update
@@ -26,6 +40,11 @@ class AlbumsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+  def album_params
+    params.require(:album).permit(:album_name, :album_type, :band_id)
   end
 
 end
