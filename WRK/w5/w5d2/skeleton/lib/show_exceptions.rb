e599ccc22d714c4d6110f50a39fd637e32c0a175
Joseph Katz <jmk742@nyu.edu>
2016-09-14 16:50:34 -0400

    Add Exceptions

--- WRK/w5/w5d2/skeleton/lib/show_exceptions.rb
+++ WRK/w5/w5d2/skeleton/lib/show_exceptions.rb
@@ -1,10 +1,21 @@
 require 'erb'
-
+require 'byebug'
 class ShowExceptions
   def initialize(app)
+    @app = app
   end
+  attr_reader :app
 
   def call(env)
+    begin
+      app.call(env) # What is this doing. It is trying to do something illegal, but why is the env passed in
+    rescue Exception => e
+      @exception = e
+      render_exception(e)
+      path = "lib/templates/rescue.html.erb"
+      html = ERB.new(File.read(path)).result(binding)
+      return ["500", {"Content-type"=>"text/html"}, [html]]
+    end
   end
 
   private
