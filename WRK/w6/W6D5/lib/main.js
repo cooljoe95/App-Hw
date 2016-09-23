273f080bcfb58ce5f0645d07b921341d7560a41b
Joe and Phil <jmk742@nyu.edu>
2016-09-23 18:13:18 -0400

    phase 4 complete

--- WRK/w6/W6D5/lib/main.js
+++ WRK/w6/W6D5/lib/main.js
@@ -21,11 +21,70 @@ window.$l = function(element){
   }
 };
 
-$l( () => {
-  let a = $l('button');
+window.$l.extend = function(first, ...rest) {
+  for (let i = 0; i < rest.length; i++) {
+    const hash = rest[i];
+    Object.keys(hash).forEach ((key) => {
+      first[key] = hash[key];
+    });
+  }
+};
 
-  // $l(a).on("click", (e)=>sayHi);
-  // $l(a).off("click", (e)=>sayHi);
+
+window.$l.ajax = function(options){
+  function defaultResponse(){
+    console.log(xhr.status); // for status info
+    console.log(xhr.responseType); //the type of data that was returned
+    console.log(xhr.response);
+  }
+  let defaults = {};
+  defaults["type"] = 'GET';
+  defaults["dataType"] = 'json';
+  defaults["success"] = defaultResponse;
+  defaults["url"] = './';
+  defaults["error"] = defaultResponse
+  defaults["data"] = '""';
+  const xhr = new XMLHttpRequest();
+  // debugger
+  $l.extend(defaults, options);
+  xhr.open(`${options.type}`, `${options.url}`);
+
+  xhr.onload = function(){
+    console.log(xhr.status); // for status info
+    console.log(xhr.responseType); //the type of data that was returned
+    console.log(xhr.response);
+  };
+
+  const optionalData = {
+    dataType: `${options.dataType}`,
+    data: `${options.data}`
+  };
+
+  xhr.send(optionalData);
+};
+
+$l.ajax({
+  type: 'GET',
+  url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  success(data) {
+    console.log("We have your weather!");
+    console.log(data);
+  },
+  error() {
+    console.error("An error occurred.");
+  },
+});
+
+function sayHi(){
+  console.log("HI");
+}
+$l( () => {
+  // debugger
+  let a = $l('button');
+  debugger
+
+  $l(a).on("click", sayHi);
+  // $l(a).off("click", sayHi);
 });
   // document.querySelectorAll()
   // debugger
